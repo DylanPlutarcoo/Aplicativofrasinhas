59f289e45976d6b584ae40fc1491a7d6a7735d11
DylanSkywalker <gplutarco02@gmail.com>
2023-05-18 21:24:31 -0300

    Initial Commit

---
+++ aplicativo/aplicativoApp.swift
@@ -0,0 +1,17 @@
+//
+//  aplicativoApp.swift
+//  aplicativo
+//
+//  Created by userext on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct aplicativoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
